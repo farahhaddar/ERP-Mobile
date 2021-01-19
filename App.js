a014817036b236f5a38f1ea881aa7249719f0537
ahazimeh <hazimeh95@gmail.com>
2021-01-19 10:40:40 +0200

    ali

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./components/TableComp";
 
 export default function App() {
   const Stack = createStackNavigator();
