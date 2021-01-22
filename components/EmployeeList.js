1a80035208a42b2f54b96efe0b55e0e9db281a08
farahhaddar <farah.haddar99@gmai.com>
2021-01-22 19:16:14 +0200

    ..

--- components/EmployeeList.js
+++ components/EmployeeList.js
@@ -36,7 +36,7 @@ export default class FlatListComp extends React.Component {
   componentDidMount() {
     // this.setState({ refreshing: false });
     fetch(
-      "http://192.168.1.105:8000/api/employees/" +
+      "http://192.168.0.119:8000/api/employees/" +
         rows +
         "?page= " +
         this.state.page +
@@ -63,7 +63,7 @@ export default class FlatListComp extends React.Component {
   }
   handleMore = () => {
     fetch(
-      "http://192.168.1.105:8000/api/employees/" +
+      "http://192.168.0.119:8000/api/employees/" +
         rows +
         "?page= " +
         this.state.page +
@@ -100,7 +100,7 @@ export default class FlatListComp extends React.Component {
     this.setState({ page: 1 });
     this.setState({ search: e });
     fetch(
-      "http://192.168.1.105:8000/api/employees/" +
+      "http://192.168.0.119:8000/api/employees/" +
         rows +
         "?page= 1" +
         "&name=" +
